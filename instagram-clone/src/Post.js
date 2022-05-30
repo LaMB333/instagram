ef680a0dbd4f98b7febbb149ec94f28734b88c90
LaMB333 <jose.urueta@live.com>
2022-05-30 15:41:00 -0500

    avatar

--- instagram-clone/src/Post.js
+++ instagram-clone/src/Post.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import './Post.css';
+import  Avatar  from '@mui/material/Avatar';
+
 
 function Post() {
   return (
     <div className='post' >
+        <Avatar
+        className="post-avatar"
+        alt='LaMb_Urueta'
+        src="/static/images/avatar/1.jpg"
+        />
         <h3>Username</h3>
 
         <img className='post_image' src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Mazda_rx7_veilside.jpg/2560px-Mazda_rx7_veilside.jpg' alt=""/>
